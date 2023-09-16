7a49fbd892dbb633b83ac40de53c6b23c98fd5a5
chungTA0609 <chungle.drive.tomari@gmail.com>
2023-09-16 11:43:33 +0700

    fix some warning

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,46 +1,53 @@
 export default {
-  loading: "~/components/loading.vue",
+  loading: "~/components/Loading.vue",
   router: {
     extendRoutes(routes) {
       routes.push({
         path: "/",
-        component: "~/pages/dashboard/sales/index.vue"
+        component: "~/pages/dashboard/sales/index.vue",
       });
-    }
+    },
   },
   /*
-  ** Nuxt rendering mode
-  ** See https://nuxtjs.org/api/configuration-mode
-  */
-  mode: 'spa',
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
+  mode: "spa",
   /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
-  target: 'server',
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: "server",
   /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
+      },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
   /*
-  ** Global CSS
-  */
-  css: ["~/assets/scss/app.scss", 'quill/dist/quill.core.css', 'quill/dist/quill.snow.css', 'quill/dist/quill.bubble.css'],
+   ** Global CSS
+   */
+  css: [
+    "~/assets/scss/app.scss",
+    "quill/dist/quill.core.css",
+    "quill/dist/quill.snow.css",
+    "quill/dist/quill.bubble.css",
+  ],
   /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
   plugins: [
     "~/plugins/fireauth.js",
     "~/plugins/fakeauth.js",
@@ -55,46 +62,44 @@ export default {
     "~/plugins/quill-editor.js",
     "~/plugins/chartist.js",
     "~/plugins/vue-googlemap.js",
-    "~/plugins/string-filter"
+    "~/plugins/string-filter",
   ],
   /*
-  ** Auto import components
-  ** See https://nuxtjs.org/api/configuration-components
-  */
+   ** Auto import components
+   ** See https://nuxtjs.org/api/configuration-components
+   */
   components: true,
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://bootstrap-vue.js.org
-    'bootstrap-vue/nuxt',
-    'nuxt-i18n'
+    "bootstrap-vue/nuxt",
+    "nuxt-i18n",
   ],
   i18n: {
-    locales: ['en', 'fr', 'es', 'ar'],
-    defaultLocale: 'en',
+    locales: ["en", "fr", "es", "ar"],
+    defaultLocale: "en",
     vueI18n: {
-      fallbackLocale: 'en',
+      fallbackLocale: "en",
       messages: {
-        en: require('./locales/en.json'),
-        fr: require('./locales/fr.json'),
-        es: require('./locales/es.json'),
-        ar: require('./locales/ar.json'),
-        zh: require('./locales/zh.json')
-      }
-    }
+        en: require("./locales/en.json"),
+        fr: require("./locales/fr.json"),
+        es: require("./locales/es.json"),
+        ar: require("./locales/ar.json"),
+        zh: require("./locales/zh.json"),
+      },
+    },
   },
   /*
-  ** Build configuration
-  ** See https://nuxtjs.org/api/configuration-build/
-  */
-  build: {
-  },
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
+  build: {},
   env: {
     auth: process.env.VUE_APP_DEFAULT_AUTH,
     apikey: process.env.VUE_APP_APIKEY,
@@ -105,5 +110,5 @@ export default {
     message: process.env.VUE_APP_MESSAGINGSENDERID,
     appid: process.env.VUE_APP_APPId,
     measurement: process.env.VUE_APP_MEASUREMENTID,
-  }
-}
+  },
+};
